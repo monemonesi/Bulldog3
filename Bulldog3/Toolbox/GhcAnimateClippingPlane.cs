15cb6e7417a8e6b9b7bdb735628dceb6f83d8c16
Roberto Monesi <roberto.monesi@outlook.com>
2018-12-07 13:07:19 +0000

    Adding some tools to work with clipping planes directly in GH

---
+++ Bulldog3/Toolbox/GhcAnimateClippingPlane.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino;
+using Rhino.Geometry;
+
+namespace Bulldog3.Toolbox
+{
+    public class GhcAnimateClippingPlane : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the GhcAnimateClippingPlane class.
+        /// </summary>
+        public GhcAnimateClippingPlane()
+          : base("AnimateClippingPlane", "AnimateClippingPlane",
+              "move an existing clipping plane to a new position",
+              "Bulldog3", "CpPlane")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Clipping Plane guid", "id", "Clipping Plane guid", GH_ParamAccess.item);
+            pManager.AddPlaneParameter("New Plane", "pln", "New plane", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddPlaneParameter("New clipping plane", "cPln", "New clipping plane", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            Guid id = Guid.Empty;
+            bool successId = DA.GetData(0, ref id);
+            Plane updatedPlane = new Plane();
+            bool successPlane = DA.GetData(1, ref updatedPlane);
+            bool succesfullConversion = successId && successPlane;
+            if (succesfullConversion)
+            {
+                //define active document
+                RhinoDoc activeDoc = RhinoDoc.ActiveDoc;
+
+                //find the clipping plane
+                var rhinoObj = activeDoc.Objects.FindId(id);
+                var clippingPlane = rhinoObj as Rhino.DocObjects.ClippingPlaneObject;
+
+                //updated the plane for the clipping plane
+                clippingPlane.ClippingPlaneGeometry.Plane = updatedPlane;
+
+                clippingPlane.CommitChanges();
+            }
+
+            
+
+
+            DA.SetData(0, updatedPlane);
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("a907e56d-6ae1-4f0b-ae2b-970040a68cc6"); }
+        }
+    }
+}
---
+++ Bulldog3/Toolbox/GhcCreateClippingPlane.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino;
+using Rhino.Display;
+using Rhino.Geometry;
+
+namespace Bulldog3.Toolbox
+{
+    public class GhcCreateClippingPlane : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the GhcCreateClippingPlane class.
+        /// </summary>
+        public GhcCreateClippingPlane()
+          : base("CreateClippingPlane", "CreateClippingPlane",
+              "Transform a simple GH plane in clipping Plane:",
+              "Bulldog3", "CpPlane")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddBooleanParameter("Create clipping plane", "run", "Create the clipping plane", GH_ParamAccess.item);
+            pManager.AddPlaneParameter("Original Plane", "pln", "Starting plane location", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("ClippingPlane GUID", "guid", "Contain the Guid of the new generated clipping plane", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            bool createClippingPlane = false;
+            if (!DA.GetData(0, ref createClippingPlane)) return;
+
+            Plane referencePlane = new Plane();
+            bool success = DA.GetData(1, ref referencePlane);
+
+            Guid id = Guid.Empty;
+
+            if (success && createClippingPlane)
+            {
+                //define active document
+                RhinoDoc activeDoc = RhinoDoc.ActiveDoc;
+
+                //Get the active view
+                RhinoView currentView = activeDoc.Views.ActiveView;
+
+                //Viewport where use the clipping plane
+                Guid currentViewId = currentView.ActiveViewportID;
+
+
+                //add clipping Plane
+                id = activeDoc.Objects.AddClippingPlane(referencePlane, 1, 1, currentViewId);
+                
+            }
+
+            DA.SetData(0, id);
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("770d6b7c-58ef-4229-8ee9-5c1cc51ea54e"); }
+        }
+    }
+}
