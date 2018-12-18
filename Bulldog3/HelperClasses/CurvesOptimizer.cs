59d717d12420498626a1cca857008d5a63f4508b
Roberto Monesi <roberto.monesi@outlook.com>
2018-12-18 12:29:54 +0000

    Major refactoring for GhcAdaptiveCurveSubD components and add the CurvesOptimizer helper class

---
+++ Bulldog3/HelperClasses/CurvesOptimizer.cs
@@ -0,0 +1,45 @@
+﻿using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulldog3.HelperClasses
+{
+    /// <summary>
+    /// This class hosts a collection of optimisation method for 3D curves
+    /// </summary>
+    public static class CurvesOptimizer
+    {
+        public static void DeCasteljauCurvePts(Curve crv, double tStart, double tEnd, double tolerance, SortedList<double, Point3d> separationPts)
+        {
+            Point3d startPt = crv.PointAt(tStart);
+            Point3d endPt = crv.PointAt(tEnd);
+            Point3d middlePt = 0.5 * (startPt + endPt);
+
+            double tParam;
+            crv.ClosestPoint(middlePt, out tParam);
+
+            double distanceToCompare = crv.PointAt(tParam).DistanceTo(middlePt);
+
+            if (distanceToCompare <= tolerance)
+            {
+                if (!separationPts.ContainsKey(tStart))
+                {
+                    separationPts.Add(tStart, startPt);
+                }
+                if (!separationPts.ContainsKey(tEnd))
+                {
+                    separationPts.Add(tEnd, endPt);
+                }
+            }
+            else
+            {
+                double tMid = 0.5 * (tStart + tEnd);
+                DeCasteljauCurvePts(crv, tStart, tMid, tolerance, separationPts);
+                DeCasteljauCurvePts(crv, tMid, tEnd, tolerance, separationPts);
+            }
+        }
+    }
+}
--- Bulldog3/Optimization/GhcAdaptiveCurveSubD.cs
+++ Bulldog3/Optimization/GhcAdaptiveCurveSubD.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Bulldog3.HelperClasses;
 using Grasshopper.Kernel;
 using Rhino.Geometry;
 
 namespace Bulldog3.Optimization
 {
+    /// <summary>
+    /// Subdive a given curve using De Casteljau`s algorithm
+    /// </summary>
     public class GhcAdaptiveCurveSubD : GH_Component
     {
         /// <summary>
@@ -23,8 +26,8 @@ namespace Bulldog3.Optimization
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
-            pManager.AddCurveParameter("Curves", "c", "Curves", GH_ParamAccess.list);
-            pManager.AddNumberParameter("Threshold", "T", "Threshold", GH_ParamAccess.item, 0.01);
+            pManager.AddCurveParameter("Curves", "crvs", "Curves", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Threshold", "Thre", "Threshold", GH_ParamAccess.item, 0.01);
         }
 
         /// <summary>
@@ -32,7 +35,7 @@ namespace Bulldog3.Optimization
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            pManager.AddCurveParameter("Polylines", "pl", "REsulting Polylines", GH_ParamAccess.list);
+            pManager.AddCurveParameter("Polylines", "pl", "Resulting Polylines", GH_ParamAccess.list);
         }
 
         /// <summary>
@@ -58,47 +61,14 @@ namespace Bulldog3.Optimization
                 SortedList<double, Point3d> separationPts = new SortedList<double, Point3d>();
                 crv.Domain = new Interval(0.0, 1.0);
 
-                DeCasteljauCurvePts(crv, 0.0, 1.0, tolerance, separationPts);
+                CurvesOptimizer.DeCasteljauCurvePts(crv, 0.0, 1.0, tolerance, separationPts);
 
                 optimizedCurves.Add(new PolylineCurve(separationPts.Values));
             }
 
-
             DA.SetDataList(0, optimizedCurves);
 
         }
-
-        private void DeCasteljauCurvePts(Curve crv, double tStart, double tEnd, double tolerance, SortedList<double, Point3d> separationPts)
-        {
-            Point3d startPt = crv.PointAt(tStart);
-            Point3d endPt = crv.PointAt(tEnd);
-            Point3d middlePt = 0.5 * (startPt + endPt);
-
-            double tParam;
-            crv.ClosestPoint(middlePt, out tParam);
-
-            double distanceToCompare = crv.PointAt(tParam).DistanceTo(middlePt);
-
-            if (distanceToCompare <= tolerance)
-            {
-                if (!separationPts.ContainsKey(tStart))
-                {
-                    separationPts.Add(tStart, startPt);
-                }
-                if (!separationPts.ContainsKey(tEnd))
-                {
-                    separationPts.Add(tEnd, endPt);
-                }
-            }
-            else
-            {
-                double tMid = 0.5 * (tStart + tEnd);
-                DeCasteljauCurvePts(crv, tStart, tMid, tolerance, separationPts);
-                DeCasteljauCurvePts(crv, tMid, tEnd, tolerance, separationPts);
-            }
-        }
-
-        
 
         /// <summary>
         /// Provides an Icon for the component.
